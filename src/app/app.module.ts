20ac1fb31a8a01ae4dd25e174d1bc8f47040be55
Vincent Perrin <vpcom01@gmail.com>
2019-10-09 22:54:56 +0200

    added Material Design and mat cards to present the data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
@@ -12,6 +14,11 @@ import { AppComponent } from './app.component';
     BrowserModule,
     // import HttpClientModule after BrowserModule.
     HttpClientModule,
+    NoopAnimationsModule,
+    MatCardModule,
+  ],
+  exports: [
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
